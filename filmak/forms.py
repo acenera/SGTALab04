f179ab8fdbbc6abe866741cf397b16482074c398
acenera <acenera001@ikasle.ehu.eus>
2026-04-13 20:00:15 +0200

    internalizazioa konponduta

--- filmak/forms.py
+++ filmak/forms.py
@@ -1,18 +1,19 @@
 from django import forms
 from .models import filmak_filma
+from django.utils.translation import gettext_lazy as _
 
 
 class RegisterForm(forms.Form):
-    Erabiltzailea = forms.CharField(max_length=30, required=True)
-    Izena = forms.CharField(max_length=30, required=True)
-    Abizena = forms.CharField(max_length=30, required=True)
-    Email = forms.EmailField(max_length=75, required=True)
-    Pasahitza = forms.CharField(max_length=128, widget=forms.PasswordInput, required=True)
-    Pasahitza_Errepikatu = forms.CharField(max_length=128, widget=forms.PasswordInput, required=True)
+    Erabiltzailea = forms.CharField(max_length=30, required=True, label=_("Erabiltzailea"))
+    Izena = forms.CharField(max_length=30, required=True, label=_("Izena"))
+    Abizena = forms.CharField(max_length=30, required=True, label=_("Abizena"))
+    Email = forms.EmailField(max_length=75, required=True, label=_("Email"))
+    Pasahitza = forms.CharField(max_length=128, widget=forms.PasswordInput, required=True, label=_("Pasahitza"))
+    Pasahitza_Errepikatu = forms.CharField(max_length=128, widget=forms.PasswordInput, required=True, label=_("Pasahitza Errepikatu"))
 
 class LoginForm(forms.Form):
-    Erabiltzailea = forms.CharField(max_length=30, required=True)
-    Pasahitza = forms.CharField(max_length=128, widget=forms.PasswordInput, required=True)
+    Erabiltzailea = forms.CharField(max_length=30, required=True, label=_("Erabiltzailea"))
+    Pasahitza = forms.CharField(max_length=128, widget=forms.PasswordInput, required=True, label=_("Pasahitza"))
 
 class BozkatuForm(forms.Form):
     filmak = forms.ModelMultipleChoiceField(
